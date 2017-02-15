49aebdee8060ba48412f2553c881096e99e46285
jmarkman <jmarkman446@gmail.com>
2017-02-15 13:42:08 -0500

    dan grabbed some image from the internet and said to make it the icon

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['master.py'])
+setup(
+    windows = [{
+        'script': 'master.py',
+        "icon_resources": [(1, "pycel.ico")],
+    }],
+    options = {
+        "py2exe":{
+            "unbuffered": True,
+            "optimize": 2,
+            "bundle_files": 1,
+        }
+    }
+)
